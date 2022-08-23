0f7c5c3582d90a68bd91047298ef504a535a84de
Robert Low <robert.low91@gmail.com>
2022-08-23 16:47:43 +0100

    Task list completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :task
+  get 'tasks/:id', to: 'tasks#show', as: :show
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
