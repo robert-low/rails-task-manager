e5799305c013c90f92c9530fef6cf4067ad2bccf
Robert Low <robert.low91@gmail.com>
2023-03-22 13:17:35 +0000

    show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index' # Index page
+  get 'tasks/:id', to: 'tasks#show' # Show page
 end
