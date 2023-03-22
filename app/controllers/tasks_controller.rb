f25797e9b1c21fb5eec45383a345132a1b8cbae3
Robert Low <robert.low91@gmail.com>
2023-03-22 14:43:44 +0000

    refactored

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy] # Rails filter
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,17 +19,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -38,5 +36,9 @@ class TasksController < ApplicationController
   # strong_params for ForbiddenAttributes
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task # Finds a task
+    @task = Task.find(params[:id])
   end
 end
