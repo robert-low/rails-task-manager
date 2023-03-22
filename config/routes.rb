b20e22f08cc1166dcf93bd2c123f2077f8b42e93
Robert Low <robert.low91@gmail.com>
2023-03-22 14:18:08 +0000

    update task action

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
 
   get 'tasks/:id', to: 'tasks#show', as: :task # Show a task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # Update a task
+  patch 'tasks/:id', to: 'tasks#update'
 end
